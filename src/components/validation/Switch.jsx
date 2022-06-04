f4945c6f6ccfe9d5b9e4173092a5692468c4f5d9
El <richard.black183@gmail.com>
2022-06-04 09:13:34 +0300

    fix switch

--- src/components/validation/Switch.jsx
+++ src/components/validation/Switch.jsx
@@ -1,14 +1,31 @@
 import * as React from "react";
 import { Switch as S } from "react-native-paper";
+import { Text } from "react-native";
 
-const Switch = (onChange) => {
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+const Switch = (onChange, value, initVal = false, label) => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(() => {
+    if (value && typeof value == "boolean") {
+      console.log(value, "value");
+      return value;
+    } else if (!value && initVal) {
+      console.log(initVal, "initVal");
+      return initVal;
+    } else {
+      return false;
+    }
+  });
 
   const onToggleSwitch = () => {
     setIsSwitchOn(!isSwitchOn);
-    onChange(isSwitchOn);
+    typeof onChange == "function" ? onChange(isSwitchOn) : null;
   };
 
-  return <S value={isSwitchOn} onValueChange={onToggleSwitch} />;
+  return (
+    <>
+      {label && <Text>{label}</Text>}
+      <S value={isSwitchOn} onValueChange={onToggleSwitch} />
+    </>
+  );
 };
+
 export default Switch;
