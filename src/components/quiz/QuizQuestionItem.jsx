ae8ecf41a6f9a293b4832c55582a7ab96435470d
ai-strings <adrstrings@gmail.com>
2022-08-10 21:52:34 +0300

    !wip results

--- src/components/quiz/QuizQuestionItem.jsx
+++ src/components/quiz/QuizQuestionItem.jsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 import useStore from "../../hooks/useStore";
 
 export default function QuizQuestionItem({ data, next }) {
+    let [result] = useStore("result");
+   useEffect(() => {
+       const resultId = result?.result?.id;
+       if (!resultId) {
+           await result.create(result.result.id, preData);
+       }
+   })
+
   let mappedAnswers = data?.answers
     ? data.answers.map((item, index) => {
-        return <Answer answer={item} key={index} next={next} />;
+        return <Answer answer={item} key={index} next={next} resultId={} quizId={} />;
       })
     : null;
 
   return <>{mappedAnswers}</>;
 }
 
-function Answer({ answer, next }) {
-  let [result] = useStore("result");
+function Answer({ answer, next, resultId, quizId }) {
+
   let setQuiestionResult = async (answer) => {
     let preData = {};
+
 
     // if(answer?.right){
     //   preData
 
     // }
 
-    answer?.right
-      ? (preData.right = result?.result?.right
-          ? [...result.result.right, answer.id]
-          : [answer.id])
-      : (preData.wrong = result?.result?.wrong
-          ? [...result.result.wrong, answer.id]
-          : [answer.id]);
+    // answer?.right
+    //   ? (preData.right = result?.result?.right
+    //       ? [...result.result.right, answer.id]
+    //       : [answer.id])
+    //   : (preData.wrong = result?.result?.wrong
+    //       ? [...result.result.wrong, answer.id]
+    //       : [answer.id]);
+    // тяжело читать :(
+
+    if (answer?.right) {
+      preData.right = result?.result?.right
+        ? [...result.result.right, answer.id]
+        : [answer.id];
+    } else {
+      preData.wrong = result?.result?.wrong
+        ? [...result.result.wrong, answer.id]
+        : [answer.id];
+    }
 
     await result.update(result.result.id, preData);
   };
