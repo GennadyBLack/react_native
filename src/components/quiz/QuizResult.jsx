b8f024dcb5dcd02540756e927f2e84cc814a3996
aikhsanov <adrstrings@gmail.com>
2022-08-18 09:34:57 +0300

    !problem with result.result in QuizResult.jsx

--- src/components/quiz/QuizResult.jsx
+++ src/components/quiz/QuizResult.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useStore from "../../hooks/useStore";
 
 export default function QuizResult() {
   const [result] = useStore("result");
-  console.log(result.result, "result");
+  const [fullResult, setFullResult] = useState(null);
+  useEffect(() => {
+    const getResult = async () => {
+      const res = await result.get(result?.result?.id);
+      console.log(res, "RES");
+      setFullResult(res.data);
+    };
+    getResult();
+  }, []);
+  console.log(fullResult);
+
   return <h1>HI THERE IT's a RESULT</h1>;
 }
