b495c5139282b50c05c404252f24a94fe39d8d5f
El <richard.black183@gmail.com>
2022-10-01 11:17:25 +0600

    +start srloll

---
+++ src/components/base/ScrollPageComponent.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import {
+  View,
+  Pressable,
+  Text,
+  Dimensions,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
+import { observer } from "mobx-react-lite";
+import useStore from "../../hooks/useStore";
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  interpolate,
+} from "react-native-reanimated";
+
+const HEADER_MAX = 200,
+  HEADER_MIN = 10;
+const ScrollPageComponent = ({ data, children }) => {
+  const translateY = useSharedValue(0);
+
+  const rHeaderStyle = useAnimatedStyle(() => {
+    const height = interpolate(
+      translateY.value,
+      [0, HEADER_MAX - HEADER_MIN],
+      [HEADER_MAX, HEADER_MIN]
+    );
+    return { height: height };
+  });
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      translateY.value = e.contentOffset.y;
+    },
+  });
+  return (
+    <SafeAreaView>
+      <Animated.View style={[rHeaderStyle, styles.header]}>
+        <Text>Hi</Text>
+      </Animated.View>
+      <Animated.ScrollView
+        style={data?.wrapper_style ?? {}}
+        scrollEventThrottle={16}
+        onScroll={scrollHandler}
+      >
+        {children}
+      </Animated.ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const ScrollPageHeader = () => {};
+
+const styles = StyleSheet.create({
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+});
+export default observer(ScrollPageComponent);
--- src/components/feed/FeedItem.jsx
+++ src/components/feed/FeedItem.jsx
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, StyleSheet, Share, Image } from "react-native";
 import Animated from "react-native-reanimated";
+import { apiUrl } from "../../api";
 
 const FeedItem = ({ feed, navigation }) => {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          "React Native | A framework for building native apps using React",
+        url: "google.com",
+        title: "see on my nutc",
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   const feed_footer_menu = () => {
     const menu = [
       { title: "Like", onPress: () => {}, icon: "" },
-      { title: "Comments", onPress: () => {}, icon: "" },
-      { title: "Share", onPress: () => {}, icon: "" },
+      {
+        title: "Comments",
+        onPress: () => {
+          navigation.navigate("feed_current", { id: feed?.id });
+        },
+        icon: "",
+      },
+      {
+        title: "Share",
+        onPress: () => {
+          onShare();
+        },
+        icon: "",
+      },
     ];
     return menu.map((item) => {
       return (
-        <Pressable onPress={item.onPress}>
+        <Pressable onPress={item.onPress} key={item.title}>
           <View>
             <Text>{item.title}</Text>
           </View>
@@ -26,21 +61,38 @@ const FeedItem = ({ feed, navigation }) => {
         <View style={styles.header_info}>
           <View style={styles.author_img}></View>
           <View style={{ marginLeft: 10 }}>
-            <View style={styles.author_name}><Text>Aydar</Text></View>
-            <View style={styles.feed_created_at}><Text>12.02.2022</Text></View>
+            <View style={styles.author_name}>
+              <Text>Aydar</Text>
+            </View>
+            <View style={styles.feed_created_at}>
+              <Text>12.02.2022</Text>
+            </View>
           </View>
         </View>
-        <View style={styles.header_menu}><Text>:::</Text></View>
+        <View style={styles.header_menu}>
+          <Text>:::</Text>
+        </View>
       </View>
       {/* header end */}
       {/* title */}
-      <View style={styles.feed_title}><Text>{feed?.title}</Text></View>
+      <View style={styles.feed_title}>
+        <Text>{feed?.title}</Text>
+      </View>
       {/* title */}
       {/* image */}
-      <View style={styles.feed_image}>
-        {/* image */}
-        {/* footer */}
-      </View>
+      <Pressable
+        style={styles.feed_image}
+        onPress={() => {
+          navigation.navigate("feed_current", { id: feed?.id });
+        }}
+      >
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: `${apiUrl}/files/${feed?.path || "placeholder.png"}` }}
+        ></Image>
+      </Pressable>
+      {/* image */}
+      {/* footer */}
       <View style={styles.feed_footer}>{feed_footer_menu()}</View>
       {/* footer */}
     </Animated.View>
@@ -54,6 +106,7 @@ export default FeedItem;
 const styles = StyleSheet.create({
   feed_wrapper: {
     padding: 5,
+    overflow: "visible",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 20 },
     shadowOpacity: 0.4,
@@ -61,6 +114,7 @@ const styles = StyleSheet.create({
     elevation: 10,
     borderRadius: 10,
     height: 350,
+    zIndex: 999,
     marginBottom: 10,
   },
   header_info: {
@@ -99,7 +153,7 @@ const styles = StyleSheet.create({
     color: "black",
   },
   feed_image: {
-    backgroundColor: "grey",
+    // backgroundColor: feed?.path ? "" : "#b7b5b5",
     height: 200,
   },
   feed_footer: {
