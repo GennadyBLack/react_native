8884e15acafd33c09d1693bd88631e33fbda9493
aikhsanov <adrstrings@gmail.com>
2022-10-03 14:57:14 +0300

    +add some code to camera

--- src/screens/Cam.jsx
+++ src/screens/Cam.jsx
@@ -1,65 +1,91 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraType, getSupportedRatiosAsync } from "expo-camera";
+import { useRef, useState } from "react";
+import {
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const { height, width } = Dimensions.get("window");
+const screenRatio = height / width;
 
 export default function Cam() {
-    const [type, setType] = useState(CameraType.back);
-    const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const camera = useRef(null);
+  const [isRatioSet, setIsRatioSet] = useState(false);
 
-    if (!permission) {
-        // Camera permissions are still loading
-        return <View />;
+  const setCameraReady = async () => {
+    if (!isRatioSet) {
+      await prepareRatio();
     }
+  };
 
-    if (!permission.granted) {
-        // Camera permissions are not granted yet
-        return (
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission} title="grant permission" />
-            </View>
-        );
-    }
+  console.log(avaliableSizes, "avaliable");
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
 
-    function toggleCameraType() {
-        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-    }
-
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
     return (
-        <View style={styles.container}>
-            <Camera style={styles.camera} type={type}>
-            </Camera>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-                    <Text style={styles.text}>Flip Camera</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
     );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={(camera) => setCameraReady(camera)}
+      ></Camera>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    camera: {
-        width: "100%",
-        height: 550,
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        backgroundColor: 'transparent',
-        margin: 64,
-    },
-    button: {
-        flex: 1,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    width: "100%",
+    height: 550,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
 });
